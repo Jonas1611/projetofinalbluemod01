cfd1041c1a89abf52095943c7d0927e069cde6dc
RobferCarvalho <97922726+RobferCarvalho@users.noreply.github.com>
2022-02-10 20:30:51 -0300

    Update pro4.js

--- pro4.js
+++ pro4.js
@@ -1,39 +1,226 @@
 const prompt = require("prompt-sync")();
 
-const periodo = []; //Momentos do dia da história
 
-periodo[0] = "pela manhã";
-periodo[1] = "a tarde";
-periodo[2] = "a noite";
-periodo[3] = "pela madrugada";
+console.log(`'Bruxo' conseguiu entender seu ultimo feito e agora passa por uma fase nova ao qual jamais havia vivido; uma era de paz e tranquilidade. 
+Na qual tentará viver uma vida "normal" ou menos "perturbada".
 
-const status = [];
+Nessa aventura, Crowster será interpretado por você e todas as ações escolhidas por você usuario.
 
-status[0] = "Revigorado";
-status[1] = "Descansado";
-status[2] = "Disposto";
-status[3] = "Sentindo-se bem";
-status[4] = "Alimentado";
-status[5] = "Cansado";
-status[6] = "Debilitado";
-status[7] = "Indisposto";
-status[8] = "Sede";
-status[9] = "Fome";
-status[10] = "Frio";
-status[11] = "Calor";
+Bruxo apos salvar o mundo, volta para sua antiga vila para ajudar na reconstrução total, ele se sentia culpado e preferiu ajeitar as coisas.
+Conseguiu uma barraca modesta perto da entrada da vila para que conseguisse ver e ouvir os perigos possiveis chegando antes de todos e ao 
+mesmo tempo estar pronto para o dia de trabalho antes de todos. Sua rotina começa as 4:30h da manhã, indo até as 19:20h da tarde e não para 
+nem aos domingos. de 19:30h até o dia seguinte ele se resguarda para seus afazeres domesticos, treinamentos de magia e combate, lazer e outras coisas que 
+desejar fazer, lembrando que deverá dormir pelo menos 5h por dia.
 
-const humor = [];
+Neste ponto você passa a controlar 'bruxo', decidindo suas proximas ações atravéz de escolhas onde a cada decisão tomada, mudará seu final. Você precisará
+fazer escolhas que vão fazer o tempo passar, dependendo de cada tarefa o tempo passará mais rapido ou mais devagar, sendo que cada minima terefa gastará 30min
+e o maximo 5h, sendo que será impossivel desfazer uma ação depois de executada, levando a proxima escolha até que o jogo tenha um fim ou você conclusa seus objetivos
+principais. Caso prefira não usar seu nome, o nome do personagem será por padrão Crowster. 
 
+Principais objetivo:
+1- Ajudar na reconstrução da vila.
+2- Tornar-se um bruxo Especialista ou Master.
+3- Dominar Alquimia e Herbalismo.
+4- Dominar todos os quatro elementos(água, terra, fogo, vento).
+
+Objetivos secundarios:
+1- Aprender uma nova profissão.
+2- Receba um titulo.
+3- Consiga ao menos um item especial ou exclusivo.
+4- Ache um amuleto de poder(anel, cordão ou brinco).
+5- Mate ou cace uma criatura Alfa.
+6- Ajude alguém.
+7- Converse com o sabio.
+8- Torne-se guardião da vila.   `);
+
+nomeBruxo:
+armadura=
+moeda= Xouro, Xprata, Xbronze, Xcobre, Xraridade.:
+energia= 
+humor=
+armadura=
+armas1=
+armas2=
+mochila=
+    
+const cicloDia = ['manhã', 'tarde', 'noite'];
+const periodoMan = [manha]; //Momentos do dia da história
+const periodoTar = [tarde];
+const periodoNoi = [noite];
+
+const moeda = [ouro, prata, bronze, cobre, raridades];
+let ouro = 2;
+let prata = 5;
+let bronze = 10;
+let cobre = 50;
+let rar = 0;
+
+const energia = [];
+const sede = [];
+const fome = [];
+const vigor = [];
+const atençao = [];
+const vida = [];
+
+const manha = ['4:30', '5', '5:30', '6' , '6:30', '7','7:30', '8', '8:30', '9', '9:30', '10', '10:30', '11', '11:30', '12'];
+const tarde = ['12:30', '13', '13:30', '14', '14:30', '15', '15:30', '16', '16:30', '17', '17:30', '18', '18:30', '19'] 
+const noite = ['19:30', '20', '20:30', '21', '21:30', '22', '22:30', '23', '23:30', '00', '00:30', '1','1:30', '2', '2:30', '3', '3:30', '4'];
+
+const titulos = ['realeza', 'plebeu', 'coletor', 'fazendeiro', 'caçador', 'chefe', 'alto', 'sumo', 'baixo'];
+const profissão = ['ferreiro', 'alquimia', 'herbalismo', 'medico', 'cozinheiro', 'joalheiro', 'minerador'];
+
+const mochila = [armadura, armas, moeda, itens];
+
+/*
+nivel(inicio) dos pers.:(ajudante, estudando, aprendiz)
+nivel(normal): denominação de cada um
+nivel(expert):(aperfeiçoado, disciplinado, dominador, mestre prof)
+nivel(unico):(Especialista, Master)
+obs: sabio e guardião não possuem nivel pois são de posições muito singulares e unicas, e apenas ladrões, assassinos e hunters podem obter os niveis de
+mercenário*/
+
+const armas = [armaLonga, armaCurta, armaRanged];
+const armadura = ['cabeça', 'ombros', 'peitoral', 'luvas', 'cinto', 'calça', 'sapato']; 
+const amuleto = ['anel', 'cordão', 'brinco'];
+const armaLonga = ['espada', 'lança', 'maça', 'machado', 'martelo', 'bastão'];
+const armaCurta = ['espada', 'adaga', 'faca serimonial', 'punhal', 'glave'];
+const armaRanged = ['arco', 'besta', 'cajado', 'facas de arremeço'];
+
+/*niveis dos itens:([comum, basico, essencial,] [exclusivo, raro, ultra raro,] [epico, especial,] exclusivo.)
+itens/armas exclusivas são feitas por ferreiros ou joalheiros*/
+
+
+
+
+
+const sttsVital = [vida, energia, fome, sede, vigor, atençao, humor];
+
+const status1 = [];  //stts para fome, vida, sede, atenção e energia
+status1[0] = "excelente";
+status1[1] = "bom";
+status1[2] = "medio";
+status1[3] = "baixo";
+status1[4] = "pessimo";
+
+const status2 = []; //stts para vigor(estado fisico do perso)
+status2[0] = "ileso";
+status2[1] = "contundido";
+status2[2] = "machucado";
+status2[3] = "sangrando";
+status2[4] = "gravemente ferido"
+
+const status3 = [];  //stts para humor
 humor[0] = "Bom";
 humor[1] = "Mau";
 humor[2] = "Normal";
-humor[3] = "Estrssado";
+humor[3] = "Estressado";
 humor[4] = "Excelente";
 
 const bruxo = {
-  nome: `José`,
-  idade: 20,
-};
+    nome : `José`,
+    idade: 20,
+}
+
+if(escolha === 1){
+
+}else if(escolha === 2){
+
+}else if(escolha === 3){
+
+}else if(escolha === 4){
+
+}else if(escolha === 5){
+
+}else{
+    console.log();
+}
+
+
+if(escolha === 1){
+
+}else if(escolha === 2){
+
+}else if(escolha === 3){
+
+}else if(escolha === 4){
+
+}else if(escolha === 5){
+
+}else{
+    console.log();
+}
+
+
+
+
+if(escolha === 1){
+
+}else if(escolha === 2){
+
+}else if(escolha === 3){
+
+}else if(escolha === 4){
+
+}else if(escolha === 5){
+
+}else{
+    console.log();
+}
+
+
+
+
+if(escolha === 1){
+
+}else if(escolha === 2){
+
+}else if(escolha === 3){
+
+}else if(escolha === 4){
+
+}else if(escolha === 5){
+
+}else{
+    console.log();
+}
+
+
+
+
+if(escolha === 1){
+
+}else if(escolha === 2){
+
+}else if(escolha === 3){
+
+}else if(escolha === 4){
+
+}else if(escolha === 5){
+
+}else{
+    console.log();
+}
+
+
+
+
+if(escolha === 1){
+
+}else if(escolha === 2){
+
+}else if(escolha === 3){
+
+}else if(escolha === 4){
+
+}else if(escolha === 5){
+
+}else{
+    for(){
+        
+    }
+}
+}
+
 
 function momentoAleatorio() {
   const randompro4 = Math.floor(Math.random() * periodo.length);
@@ -42,28 +229,16 @@ function momentoAleatorio() {
   return momentoInicial;
 }
 
-console.log(`O Bruxo`); //será alterado depois
-
-console.log(`
-"O Bruxo" conseguiu entender seu último feito, agora passa 
-por uma fase nova, ao qual jamais viveu; uma era de paz e 
-tranquilidade, na qual tentará viver uma vida "normal" ou 
-menos" perturbada".
-
-Nessa aventura, 'bruxo' será interpretado por você, escolha 
-todas as suas ações.
-`);
+const nome = prompt(`Digite seu nome ou deixe em branco para padrão`);
+if (nome = void){
+    console.log('nome padrão escolhido')
+}else{
+    console.log(`${nome} será o nome do bruxo.);  //será alterado depois
 
 const nome = prompt("Digite seu nome para iniciar: "); // Jogador colocará seu nome para haver um rappot entre o jogo e o jogador
 
-console.log(`O bruxo ${nome} após salvar o mundo, volta para
-sua antiga vila e ajuda na reconstrução total, ele sentia - se
-culpado e preferiu ajeitar às coisas.
-  
-Conseguiu uma barraca modesta, perto da entrada da vila, para que
-conseguisse ver e ouvir às possíveis ameaças chegando antes de todos,
-e ao mesmo tempo, estar pronto para o dia de trabalho antes de todos.
+console.log(``)
 
-Sua rotina começa às 4: 30h da manhã, indo até às 19: 20h da tarde,
-não para nem aos domingos.Das 19: 30h até o dia seguinte ele se resguarda
-para seus afazeres domésticos, treinamentos de magía e combate.`)
+
+
+
